1eb85093a85bcc330b3992392f59a8e7d37c16ff
kkin1995 <karan@akinariwala.com>
2020-12-29 18:05:02 +0530

    Implemented a program to plot the phase space for a given set of angles

---
+++ simple-pendulum/phase_space.py
@@ -0,0 +1,24 @@
+from simple_pendulum import SimplePendulum
+import matplotlib.pyplot as plt
+import sys
+
+def multiple_phase_plot(initialAngles):
+    if type(initialAngles) is not list:
+        initialAngles = [initialAngles]
+
+    for initialAngle in initialAngles:
+        pend = SimplePendulum(initialAngle = initialAngle)
+        _, theta, omega = pend.non_linear_rk4()
+        plt.plot(theta, omega, label = str(initialAngle) + " degrees")
+
+    plt.title("Phase Space Plot")
+    plt.xlabel(r"$\theta$")
+    plt.ylabel(r"$\dot{\theta}$")
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    initialAngles = []
+    for element in sys.argv[1:]:
+        initialAngles.append(int(element))
+    multiple_phase_plot(initialAngles)
