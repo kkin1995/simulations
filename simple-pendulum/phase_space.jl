9d231604667806fc9674de4c87842ec816458d72
kkin1995 <karan@akinariwala.com>
2020-12-29 19:31:47 +0530

    Implemented a Julia script for the phase space plot for a simple pendulum

---
+++ simple-pendulum/phase_space.jl
@@ -0,0 +1,26 @@
+include("./simple-pendulum.jl")
+using .SimplePendulumSimulation
+using Plots
+
+function phase_plot(initialAngles)
+    plot_list = Any[]
+    for initialAngle in initialAngles
+        println(initialAngle)
+        theta, omega, _ = SimplePendulum(initialAngle)
+        title = "Phase Space Plot"
+        label = string(initialAngle) * " degress"
+        push!(plot_list, plot(theta, omega, title = title, label = label))
+    end
+    xlabel!("Theta")
+    ylabel!("Theta Dot")
+    return plot_list
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    initialAngles = 5
+    plot_list = phase_plot(initialAngles)
+    savefig(plot(plot_list...), "phase.png")
+end
+
+end
