559d36981075fed8eb19277abc50f95619017692
kkin1995 <karan@akinariwala.com>
2021-01-08 15:22:46 +0530

    Added theta and omega to return statement

--- simple-pendulum/phase_space.py
+++ simple-pendulum/phase_space.py
@@ -2,12 +2,12 @@ from simple_pendulum import SimplePendulum
 import matplotlib.pyplot as plt
 import sys
 
-def multiple_phase_plot(initialAngles, plotting = False):
+def multiple_phase_plot(initialAngles, L = 1, g = 9.8, cycles = 10, step_size = 0.001, plotting = False):
     if type(initialAngles) is not list:
         initialAngles = [initialAngles]
 
     for initialAngle in initialAngles:
-        pend = SimplePendulum(initialAngle = initialAngle)
+        pend = SimplePendulum(initialAngle = initialAngle, L = L, g = g, cycles = cycles, step_size = step_size)
         _, theta, omega = pend.non_linear_rk4()
         if plotting:
             plt.plot(theta, omega, label = str(initialAngle) + " degrees")
@@ -18,6 +18,8 @@ def multiple_phase_plot(initialAngles, plotting = False):
         plt.ylabel(r"$\dot{\theta}$")
         plt.legend()
         plt.show()
+
+    return theta, omega
 
     
 
